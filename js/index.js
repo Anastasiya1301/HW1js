cde6526c2a6d98568ac1c4e7bb2a135e4eb5ea92
Anastasiya1301 <dudkoanastasiya1301@gmail.com>
2025-05-10 15:38:06 +0300

    js

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let age = 15;
+console.log(age);
+const name = "Nastya";
+console.log(name);
+const isStudent = true;
+console.log(isStudent);
+const myString = "Будьте з тими людьми, які змушують вас посміхатися у важкі часи";
+console.log(myString);
+let myNumber = 10;
+myNumber = myNumber + 40;
+myNumber + 10;
+console.log(myNumber);
+const myNull = null;
+console.log(myNull);
+// const newMessage = "Введіть своє ім'я"
+// prompt(newMessage);
+// alert(newMessage);
+// console.log(newMessage);
+// console.log(typeof newMessage);
+const message = "Ви дійсно хочете покинути сторінку?";
+const isConfirmed = confirm(message);
+console.log(isConfirmed);
+const warningMessage = "Увага! Дія може бути небезпечною.";
+alert()
