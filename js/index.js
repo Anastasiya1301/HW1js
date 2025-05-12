259f0a1cd13921ab0cf39fb9231dfbc2d8bee318
Anastasiya1301 <dudkoanastasiya1301@gmail.com>
2025-05-12 19:14:52 +0300

    виправлення помилок

--- js/index.js
+++ js/index.js
@@ -1,26 +1,42 @@
-let age = 15;
+//1
+let age = 16;
 console.log(age);
+//2
 const name = "Nastya";
 console.log(name);
+//3
 const isStudent = true;
 console.log(isStudent);
-const myString = "Будьте з тими людьми, які змушують вас посміхатися у важкі часи";
+//4
+let myString = "Будьте з тими людьми, які змушують вас посміхатися у важкі часи";
 console.log(myString);
+//5
 let myNumber = 10;
 myNumber = myNumber + 40;
 myNumber + 10;
 console.log(myNumber);
+//6
 const myNull = null;
 console.log(myNull);
-const newMessage = "Введіть своє ім'я"
-prompt(newMessage);
-alert(newMessage);
-console.log(newMessage);
+//7
+const newMessage = prompt("Введіть своє ім'я");
+console.log(newMessage)
 console.log(typeof newMessage);
-const message = "Ви дійсно хочете покинути сторінку?";
-const isConfirmed = confirm(message);
-console.log(isConfirmed);
-const warningMessage = "Увага! Дія може бути небезпечною.";
-alert(warningMessage);
-const confirmedMessage = "Ви впевнені, що хочете продовжити?";
-const isConfirm = confirm(confirmedMessage);
+alert(`Привіт, ${newMessage}!`);
+//8
+const message = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(typeof message);
+if (message === true) {
+  alert("Дякую за підтвердження!");
+} else {
+  alert("Дію відмінено!");
+}
+//9
+alert(" Увага! Дія може бути небезпечною. ");
+const warningMessage = confirm("Ви впевнені що хочете продовжити?");
+
+if (warningMessage === true) {
+  alert("Дякую за підтвердження!");
+} else {
+  alert("Дію відмінено!");
+}
